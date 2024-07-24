43624cacaad2482af2af4216104675995eff5067
Kristal777 <bryan500cardenas@gmail.com>
2024-07-24 18:03:00 -0500

    Examen

---
+++ src/Controllers/EstudianteController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+public class EstudianteController {
+
+    public procesarEstudiante(Estudiante[]estudiante):HashMap{
+
+    }
+
+    private obtenerCategoria(int calificacion):{
+        
+    }
+
+}
---
+++ src/Controllers/Models/Estudiantes.java
@@ -0,0 +1,5 @@
+package Controllers.Models;
+
+public class Estudiantes {
+
+}
